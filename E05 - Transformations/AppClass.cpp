e052204a76602835746c39a9b8f0a5625c0628c6
Diana Diaz <dxd9318@g.rit.edu>
2019-09-23 22:06:11 -0400

    Notes to self in AppClass.cpp for planning out E05

--- E05 - Transformations/AppClass.cpp
+++ E05 - Transformations/AppClass.cpp
@@ -30,23 +30,34 @@ void Application::Display(void)
 	// Clear the screen
 	ClearScreen();
 
-	m_pMesh->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), ToMatrix4(m_qArcBall));
-	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3( 3.0f, 0.0f, 0.0f)));
-
-	/*
+	/*	
+	//Taken from C11 Translation and Scale demo
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
 
+	// SCALE
 	matrix4 m4Scale = glm::scale(IDENTITY_M4, vector3(2.0f, 2.0f, 2.0f));
-	static float value = 0.0f;
+	static float value = 0.0f;	// Counter, static to increment per display iteration
+	// TRANSLATION
 	matrix4 m4Translate = glm::translate(IDENTITY_M4, vector3(value, 2.0f, 3.0f));
-	value += 0.01f;
+	value += 0.01f;				// Will only increment after mesh has been translated in this instance
 
-	//matrix4 m4Model = m4Translate * m4Scale;
+	//matrix4 m4Model = m4Translate * m4Scale;	//backwards multiplication **********DO NOT USE***********
 	matrix4 m4Model = m4Scale * m4Translate;
 
 	m_pMesh->Render(m4Projection, m4View, m4Model);
+
+	//whether using the above or below rendering method, final rendering will always follow this paradigm: mesh->Render(Projection matrix, View matrix, Model matrix)
 	*/
+
+	//m_pMesh->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), ToMatrix4(m_qArcBall));
+	//m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(3.0f, 0.0f, 0.0f)));
+
+	//FOR LEFT-RIGHT SPACE INVADER MOTION
+	// keep a "direction" variable, set equal to 1
+	// multiply translation value by "direction" variable
+	// whenever counter hits absVal of 10 (or some other value), multiply "direction" var by -1 (flip translation direction)
+	// MUST APPLY THIS LOGIC TO EVERY CUBE? I think that should be fine (ie, not too costly)
 
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
