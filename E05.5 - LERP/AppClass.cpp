4464b106504df27eb4b313d925c1a2db62208189
diana_diaz <diana.diaz@eagledream.com>
2019-10-01 14:27:32 -0400

    E05.5 completed. Finished fleshing out lerp calculations.

--- E05.5 - LERP/AppClass.cpp
+++ E05.5 - LERP/AppClass.cpp
@@ -46,28 +46,33 @@ void Application::Display(void)
 	// Draw the model
 	m_pModel->PlaySequence();
 
-	//Get a timer
+	// Get a timer
 	static float fTimer = 0;	//store the new timer
 	static uint uClock = m_pSystem->GenClock(); //generate a new clock for that timer
 	fTimer += m_pSystem->GetDeltaTime(uClock); //get the delta time for that timer
 
-	//calculate the current position
-	static uint sprintCounter = 0;	// Keeps track of the number of the current "sprint", ie. the path between the current and next stops.
-	vector3 v3SprintStart = m_stopsList[sprintCounter];	// Sets the starting point for the current sprint based on the number of the current sprint.
-	vector3 v3SprintEnd = m_stopsList[(sprintCounter + 1) % m_stopsList.size()];	// Sets the next stop as the end of the current sprint. 
-																					// % used to loop to first stop if at end of stops list. 
-
-
-	vector3 v3CurrentPos;
-
-
-	//your code goes here
-	v3CurrentPos = vector3(0.0f, 0.0f, 0.0f);	// replace this vector3(0) with call to glm::lerp function
 	//-------------------
+	// Set current sprint number, start, and end points
+	static uint sprintCounter = 0;	// Keeps track of number of the current "sprint", ie. the path between the current and next stops.
+	vector3 v3SprintStart = m_stopsList[sprintCounter];	// Sets starting point for the current sprint, using sprintCounter as an index for the stops vector.
+	vector3 v3SprintEnd = m_stopsList[(sprintCounter + 1) % m_stopsList.size()];	/*	Sets the next stop as the end of the current sprint. 
+																						% used to loop to first stop if at end of stops vector. */
+	// Calculate percentage to LERP by
+	float fTimeBtwnStops = 1.0f;
+	float fLerpPercentage = MapValue(fTimer, 0.0f, fTimeBtwnStops, 0.0f, 1.0f);		/*	Converts the ratio of [current elapsed time(fTimer) : fTimeBtwnStops]
+																						into a percentage value, scaled between 0.0f and 1.0f. */
+	// Use LERP percentage to calculate the current position
+	vector3 v3CurrentPos = glm::lerp(v3SprintStart, v3SprintEnd, fLerpPercentage);
 	
+	// Reset timer and percentage when sprint ends (percentage == 100%)
+	if (fLerpPercentage >= 1.0f)
+	{
+		sprintCounter++;							// increment sprintCounter to set start of next sprint
+		sprintCounter %= m_stopsList.size();		// if this was the last sprint, reset the sprint number to allow looping
+		fTimer = m_pSystem->GetDeltaTime(uClock);	// reset timer to allow LERPing for next sprint
+	}
+	//-------------------
 
-
-	
 	matrix4 m4Model = glm::translate(v3CurrentPos);
 	m_pModel->SetModelMatrix(m4Model);
 
