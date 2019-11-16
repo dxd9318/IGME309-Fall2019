608764ee8d48576e7317b06e10b5230d6b351e64
dxd9318 <dxd9318@g.rit.edu>
2019-11-16 17:38:43 -0500

    A05 Octree - Finished filling out Octant.h referencing the Octant.h of C21SpatialOptimizationPt2. Comments refactored to aid in understanding, will continue to re-order some function methods for this file.

--- A05 - Octree/Octant.h
+++ A05 - Octree/Octant.h
@@ -26,18 +26,106 @@ class Octant // created based off of the Octant.h file used in the C21_SpatialOp
 	Octant* m_pParent = nullptr; // this octant's parent octant
 
 	// Shape info for constructing this octant
-	vector3 m_vMin = vector3(0.0f);	// octant's min corner (BLB)
-	vector3 m_vMax = vector3(0.0f);	// octant's max corner (TRF)
-	vector3 m_vCenter = vector3(0.0f);	// center point of the octant
-	vector3 m_vSize = vector3(0.0f); // size of the octant as a vector
+	vector3 m_v3Min = vector3(0.0f);	// octant's min corner (BLB)
+	vector3 m_v3Max = vector3(0.0f);	// octant's max corner (TRF)
+	vector3 m_v3Center = vector3(0.0f);	// center point of the octant
+	vector3 m_v3Size = vector3(0.0f); // size of the octant as a vector
 
 	// Entity Management
 	MeshManager* m_pMeshMngr = nullptr; // Mesh Manager singleton
 	MyEntityManager* m_pEntityMngr = nullptr; // Entity Manager singleton
 	std::vector<uint> m_EntityList;	// list of indices corresponding to entities currently located within this octant
 
-};	//class end
+public:
+#pragma region The Big Three, and then some
+	// Constructor 1 (Root)
+	Octant(uint a_nMaxLevel = 2, uint a_nIdealEntityCount = 5);
 
-}	//namespace Simplex end
+	// Constructor 2 (All children octants)
+	Octant(vector3 a_v3Center, vector3 a_v3Size);
+
+	// Copy Assignment Operator
+	Octant& operator=(Octant const& other);
+
+	// Destructor
+	~Octant(void);
+
+	// Swap
+	void Swap(Octant& other);
+#pragma endregion
+
+#pragma region Functions for Octant Shape Creation
+	// Returns this octant's size vector
+	vector3 GetSize(void);
+
+	// Returns this octant's center vector in global space
+	vector3 GetCenterGlobal(void);
+
+	// Returns this octant's min vector (BLB) in global space
+	vector3 GetMinGlobal(void);
+
+	// Returns this octant's max vector (TRF) in global space
+	vector3 GetMaxGlobal(void);
+#pragma endregion
+
+	// Checks if the specified entity is contained by this octant // MAYBE RENAME TO IsContained ???????
+	bool IsColliding(uint a_uRBIndex);
+	
+	// Displays the specified octant as well as all its children octants //( AM I DISPLAYING ONLY IMMEDIATE CHILDREN, OR ALL DESCENDANTS?)
+	void DisplayDescendants(uint a_nIndex, vector3 a_v3Color = C_YELLOW);
+
+	// Displays only the specified octant
+	void Display(vector3 a_v3Color = C_YELLOW);
+
+	// Displays any leaf octants that currently contain entities //(use if you want to cycle through just this type of octant)
+	void DisplayOccupiedLeaves(vector3 a_v3Color = C_YELLOW);
+
+	// clears the entity list for each octant (will do so recursively)
+	void ClearEntityList(void);
+
+	// creates 8 octant children for this octant
+	void Subdivide(void);
+
+	// returns the child octant specified by index
+	Octant* GetChild(uint a_nChild);
+
+	// returns the parent of this octant
+	Octant* GetParent(void);
+
+
+	
+
+	// Deletes all children and any further descendants
+	void KillBranches(void);
+
+	// Creates a tree through subdivision, with up to the max number of levels
+	void ConstructTree(uint a_nMaxLevel = 3);
+
+	// Assigns IDs to entities contained by leaf nodes
+	void AssignIDtoEntity(void);
+
+#pragma region Helper Functions
+	// Checks if this octant contains no child octants
+	bool IsLeaf(void);
+
+	// Checks if this octant has more than the specified number of entities
+	bool ContainsMoreThan(uint a_nEntities);
+
+	// Returns the total number of octants in the world
+	uint GetOctantCount(void);
+#pragma endregion
+
+private:
+	// Deallocates member data
+	void Release(void);
+
+	// Allocates member data
+	void Init(void);
+
+	// Creates list of all leaf octants that contain entities
+	void ListOccupiedLeaves(void);
+};	// class end
+
+}	// namespace Simplex end
 
 #endif // __OCTANT_H_
